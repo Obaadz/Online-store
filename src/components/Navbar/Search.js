abd33afdc95df451adb305e851ca303627e04062
Obada <obada8411@gmail.com>
2022-05-13 23:12:49 +0200

    Style the search bar

--- src/components/Navbar/Search.js
+++ src/components/Navbar/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoIosSearch } from "react-icons/io";
 import styled from "styled-components";
 
 const StyledSearch = styled.div`
@@ -7,10 +8,28 @@ const StyledSearch = styled.div`
 `;
 
 const StyledSearchBar = styled.input`
+  font-family: "Noto Kufi Arabic";
+  font-size: 0.75rem;
+  color: #0e5aa7;
+  height: 40px;
+  width: 400px;
+  padding-right: 10px;
+  border: none;
+  border-radius: 5px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
   ::placeholder {
-    font-family: "Noto Kufi Arabic";
-    font-size: 0.75rem;
   }
+`;
+
+const StyledSubmit = styled.button`
+  height: 40px;
+  color: white;
+  background-color: #dc2226;
+  border: none;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  cursor: pointer;
 `;
 
 const Search = () => {
@@ -22,6 +41,9 @@ const Search = () => {
   return (
     <StyledSearch>
       <form onSubmit={handleSubmit}>
+        <StyledSubmit>
+          <IoIosSearch fontSize="1.2rem" />
+        </StyledSubmit>
         <StyledSearchBar
           name="search"
           type="text"
