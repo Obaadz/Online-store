22b8c26d6bf6229a8d843fc390a1aa1d319e97d9
Obada <obada8411@gmail.com>
2022-05-23 20:09:08 +0200

    Add on hover section to the top pane of the navbar

--- src/components/Navbar/Top.js
+++ src/components/Navbar/Top.js
@@ -5,6 +5,9 @@ import nav_fastdelivery from "../../assets/images/nav_fastdelivery.svg";
 import nav_delivermore from "../../assets/images/nav_delivermore.svg";
 import nav_refund from "../../assets/images/nav_refund.svg";
 import nav_callme from "../../assets/images/nav_callme.svg";
+import nav_freedelivery from "../../assets/images/nav_freedelivery.webp";
+import nav_million_products from "../../assets/images/nav_million_products.webp";
+import nav_free_returns from "../../assets/images/nav_free_returns.webp";
 import "./verticalLine.css";
 
 const StyledTop = styled.div`
@@ -26,12 +29,57 @@ const StyledInfo = styled.div`
   display: flex;
   gap: 20px;
   & > div {
+    position: relative;
     font-size: 0.7rem;
     cursor: pointer;
 
     span {
       font-weight: 600;
       margin-right: 10px;
+    }
+
+    &:hover > div {
+      visibility: visible;
+      opacity: 1;
+      top: 40px;
+    }
+
+    & > div {
+      visibility: hidden;
+      position: absolute;
+      top: 70px;
+      background-color: white;
+      color: black;
+      display: flex;
+      gap: 30px;
+      padding: 10px;
+      border-radius: 10px;
+      box-shadow: 1px 2px 6px 0px #d3d2d294;
+      opacity: 0;
+      transition: all 0.3s;
+      cursor: auto;
+
+      img {
+        height: 120px;
+      }
+
+      div {
+        width: max-content;
+        p {
+          margin-top: 10px;
+          max-width: 30ch;
+          line-height: 1.8;
+        }
+      }
+
+      &::after {
+        content: "";
+        position: absolute;
+        border: 10px solid;
+        border-color: transparent transparent white transparent;
+        top: -20px;
+        right: 40px;
+      }
     }
   }
 `;
@@ -52,14 +100,41 @@ const Top = () => {
           <div>
             <img src={nav_fastdelivery} alt="fast delivery" />
             <span>توصيل مجاني وسريع</span>
+            <div>
+              <img src={nav_freedelivery} alt="free delivery" />
+              <div>
+                <h2>توصيل سريع</h2>
+                <p>
+                  اطلب أي وقت ومن أي مكان واستمتع بتوصيل مجاني لجميع طلباتك
+                  بقيمة 100 جنيه أو أكثر!
+                </p>
+              </div>
+            </div>
           </div>
           <div>
-            <img src={nav_delivermore} alt="fast delivery" />
+            <img src={nav_delivermore} alt="deliver more then 15k products" />
             <span>تسوق أكثر من 15000 منتج.</span>
+            <div>
+              <img src={nav_million_products} alt="million products" />
+              <div>
+                <h2>تسوق أكثر من 15000 منتج.</h2>
+                <p>
+                  تسوق أكثر من 15000 منتج تصفح مجموعة كبيرة من الأطعمة الطازجة،
+                  والبقالة والإلكترونيات والمزيد!
+                </p>
+              </div>
+            </div>
           </div>
           <div>
-            <img src={nav_refund} alt="fast delivery" />
+            <img src={nav_refund} alt="free refund" />
             <span>إرجاع مجاني</span>
+            <div>
+              <img src={nav_free_returns} alt="free returns" />
+              <div>
+                <h2>إرجاع مجاني</h2>
+                <p>إرجاع مجاني بدون متاعب لكل طلباتك وبدون أي رسوم</p>
+              </div>
+            </div>
           </div>
         </StyledInfo>
         <StyeldCallme>
