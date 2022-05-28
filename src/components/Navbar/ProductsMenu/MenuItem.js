9297c5e0986c9c4b2d96db8ab41fc9c35b5a8a6c
Obada <obada8411@gmail.com>
2022-05-28 18:24:09 +0200

    Add hover items section for the products menu

--- src/components/Navbar/ProductsMenu/MenuItem.js
+++ src/components/Navbar/ProductsMenu/MenuItem.js
@@ -1,9 +1,52 @@
 import React from "react";
+import styled from "styled-components";
+import Container from "../../Container";
 
-const MenuItem = ({ name, href }) => {
+const StyledProducts = styled(Container)`
+  display: none;
+  position: absolute;
+  top: 45px;
+  right: 0%;
+  flex-wrap: wrap;
+  flex-direction: column;
+  max-height: 70vh;
+  background-color: #fff;
+  gap: 20px;
+  padding: 20px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+`;
+
+const StyledItem = styled.ul``;
+
+const MenuItem = ({ name, href, products }) => {
+  function renderProducts() {
+    return products.map(({ name, items }) => (
+      <StyledItem key={name}>
+        <h3>{name}</h3>
+        {items.map((item) => (
+          <li key={item}>
+            <a href="#item">{item}</a>
+          </li>
+        ))}
+      </StyledItem>
+    ));
+  }
+
+  function handleHoverIn() {
+    document.documentElement.style.backgroundColor = "rgb(62, 62, 62)";
+  }
+
+  function handleHoverOut() {
+    document.documentElement.style.backgroundColor = "";
+  }
+
   return (
-    <li>
+    <li onMouseEnter={handleHoverIn} onMouseLeave={handleHoverOut}>
       <a href={href}>{name}</a>
+      {products.length > 0 && (
+        <StyledProducts>{renderProducts()}</StyledProducts>
+      )}
     </li>
   );
 };
--- src/components/Navbar/ProductsMenu/ProductsMenu.js
+++ src/components/Navbar/ProductsMenu/ProductsMenu.js
@@ -6,26 +6,31 @@ import MenuItem from "./MenuItem";
 import "../verticalLine.css";
 
 const StyledMenu = styled.menu`
+  position: relative;
   font-size: 0.8rem;
   display: flex;
   align-items: center;
 
-  li {
+  & > li {
     padding: 0.9rem 0;
-    a {
+    & > a {
       padding: 0.9rem 0.75rem;
     }
 
     &:first-child {
       font-weight: 600;
 
-      a {
+      & > a {
         padding-right: 0;
       }
     }
 
     &:hover {
       background-color: #f7f7f7;
+
+      & > div {
+        display: flex;
+      }
     }
   }
 
@@ -43,13 +48,57 @@ const StyledBg = styled.div`
 `;
 
 const items = [
-  { key: 1, href: "#drinks", name: "المشروبات" },
-  { key: 2, href: "#tablets", name: "موبايلات، تابلت وأجهزة اليد الذكية" },
-  { key: 3, href: "#electronics", name: "الإلكترونيات والأجهزة المنزلية" },
-  { key: 4, href: "#supermarket", name: "السوبر ماركت" },
-  { key: 5, href: "#food", name: "أطعمة طازجة" },
-  { key: 6, href: "#baby", name: "مستلزمات الأطفال" },
-  { key: 7, href: "#vegetables", name: "الخضار" },
+  {
+    key: 1,
+    href: "#drinks",
+    name: "المشروبات",
+    products: [
+      {
+        name: "قهوه",
+        items: ["خراطيش قهوة", "البن المطحون والعادي", "أكياس قهوة فورية"],
+      },
+      {
+        name: "العصائر",
+        items: ["عصائر طويلة الأمد", "شرابات ومحليات"],
+      },
+      {
+        name: "مشروبات الأطفال",
+        items: ["عصائر الاطفال"],
+      },
+      {
+        name: "مشروبات بودرة",
+        items: ["بودرة عصائر", "الشوكولاته الساخنة"],
+      },
+      {
+        name: "مشروبات غازية",
+        items: ["مشروبات الطاقة", "المشروبات الغازية"],
+      },
+      {
+        name: "شاي",
+        items: ["شاي مثلج", "أكياس الشاي و الأوراق السائبة", "أكياس الشاي"],
+      },
+      {
+        name: "ماء",
+        items: ["المياه المنكهة", "مياه فوارة", "مياه معدنية", "مياه عادية"],
+      },
+    ],
+  },
+  {
+    key: 2,
+    href: "#tablets",
+    name: "موبايلات، تابلت وأجهزة اليد الذكية",
+    products: [],
+  },
+  {
+    key: 3,
+    href: "#electronics",
+    name: "الإلكترونيات والأجهزة المنزلية",
+    products: [],
+  },
+  { key: 4, href: "#supermarket", name: "السوبر ماركت", products: [] },
+  { key: 5, href: "#food", name: "أطعمة طازجة", products: [] },
+  { key: 6, href: "#baby", name: "مستلزمات الأطفال", products: [] },
+  { key: 7, href: "#vegetables", name: "الخضار", products: [] },
 ];
 
 const ProductsMenu = () => {
